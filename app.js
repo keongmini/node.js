a1ec1295b3f6c2e271717c57fd9bfd7d679732ee
keongmin <kmnam09@gmail.com>
2021-11-01 13:04:26 +0900

    node.js express - middleware, bodyparser and serving html

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser')
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 
-server.listen(3000);
+// use : allows add middleware
+// app.use((req, res, next) => {
+//     console.log('In the middleware')
+//     next(); // Allows the request to continue to the next middleware in line
+// });
+
+
+// app.use('/', (req,res,next)=> {
+//     console.log('This always run');
+//     next();
+// })
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
